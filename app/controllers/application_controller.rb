faf2b52141e08d9b1794713b24a7d17f845c85b3
Noah A Klem <noahaklem@gmail.com>
2021-09-24 16:02:09 -0600

    commit tree 83da209f5162102b0669cc13f0ee2c183dfa3f9d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,21 +8,27 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    erb :home
     # your code here
   end
 
   get '/registrations/signup' do
-    # your code here
+    erb :'registrations/signup'
   end
 
   post '/registrations' do
-    # your code here
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    @user.save
 
+    session[:user_id] = @user.id
+    # your code here
     redirect '/users/home'
   end
 
   get '/users/home' do
     # your code here
+    @user = User.find(session[:user_id])
+    erb :'/users/home'
   end
 
   get '/sessions/login' do
@@ -31,14 +37,17 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    # your code here
-
-    redirect '/users/home'
+    @user = User.find_by(email: params["email"], password: params["password"])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/users/home'
+    end
+    redirect '/sessions/login'
   end
 
   get '/sessions/logout' do
     # your code here
-    
+    session.clear
     redirect '/'
   end
 
